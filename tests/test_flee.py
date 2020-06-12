4913a2626bc3fd9947258a9ff97e2830cac1c492
Hamid Arabnejad <hamid.arabnejad@gmail.com>
2020-06-12 22:28:06 +0100

    Update test_flee.py

--- tests/test_flee.py
+++ tests/test_flee.py
@@ -6,8 +6,9 @@ import subprocess
 def test_fabflee_mali():
     config_path = "cd %s/FabFlee/config_files/%s;" % (
 	    os.environ['TRAVIS_BUILD_DIR'], "mali")
-    cmd = [config_path,
-	"python3",
+    current_dir = os.getcwd()
+    os.chdir(config_path)
+    cmd = ["python3",
 	"run.py",
 	"input_csv",
 	"source_data",
@@ -19,3 +20,4 @@ def test_fabflee_mali():
     assert(subprocess.call(cmd) == 0)
     output = subprocess.check_output(cmd).decode("utf-8")
     assert(output.find('success') >= 0)
+    os.chdir(config_path)
