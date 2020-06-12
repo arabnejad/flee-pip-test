1e05dac5afe961be682a98e3f6475912f5fc796c
Hamid Arabnejad <hamid.arabnejad@gmail.com>
2020-06-12 22:04:43 +0100

    Update test_flee.py

--- tests/test_flee.py
+++ tests/test_flee.py
@@ -2,37 +2,19 @@ import os
 import sys
 import subprocess
 
-sys.path.insert(0, '%s/FabSim3/base/' % (os.environ['TRAVIS_BUILD_DIR']))
-
-from deploy.templates import *
-from deploy.machines import *
-from fabric.contrib.project import *
-from base.fab import *
-
-
-def test_fabflee():
-    """
-    The main FabSim3 test suite. Every test is captured in an assert statement.
-    """
-    assert("plugins" in get_plugin_path("FabFlee"))
-    assert("FabFlee" in get_plugin_path("FabFlee"))
-    assert(len(get_fabsim_git_hash()) > 0)
-
-
-def test_fabflee_install():
-    print(os.environ['TRAVIS_BUILD_DIR'])
-    assert(subprocess.call(
-        ["fabsim", "localhost", "install_plugin:FabFlee"]) == 0)
-
 
 def test_fabflee_mali():
-    cmd = ["fabsim",
-	"localhost",
-	"flee:mali,simulation_period=50,manual_ssh=false,flee_location={0}/flee".format(
-	    os.environ['TRAVIS_BUILD_DIR'])
+    config_path = "%s/FabFlee/config_files/%s" % (
+	    os.environ['TRAVIS_BUILD_DIR'], "mali")
+    cmd = ["python3",
+	"run.py",
+	"input_csv",
+	"source_data",
+	"50",
+	"simsetting.csv"
 	]
     print(cmd)
 
-    assert(subprocess.call(cmd) == 0)
-    output = subprocess.check_output(cmd).decode("utf-8")
+    assert(subprocess.call(cmd,shell=True, cwd=config_path) == 0)
+    output = subprocess.check_output(cmd,shell=True, cwd=config_path).decode("utf-8")
     assert(output.find('success') >= 0)
